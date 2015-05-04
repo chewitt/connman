323a312000d01475279cdfd090b3c8cffe21e99a
Pasi Sjöholm <pasi.sjoholm@jollamobile.com>
2015-05-04 11:04:48 +0300

    Revert "[connman] do not start wifi scan if network is associating"
    This reverts commit e9df774158763baf301fc57b0da79b7bc39a709d.

--- connman/plugins/wifi.c
+++ connman/plugins/wifi.c
@@ -629,13 +629,9 @@ static int throw_wifi_scan(struct connman_device *device,
 	if (!wifi)
 		return -ENODEV;
 
-	struct connman_network *network = wifi->network;
-	if (!network)
-		return -ENODEV;
-
 	DBG("device %p %p", device, wifi->interface);
 
-	if (wifi->tethering || connman_network_get_associating(network))
+	if (wifi->tethering)
 		return -EBUSY;
 
 	if (connman_device_get_scanning(device))
