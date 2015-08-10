23594aa9b8573858472e156d59e31353364bcfe7
Jaakko Hannikainen <jaakko.hannikainen@intel.com>
2015-08-10 13:50:38 +0300

    service: Return OperationAborted on agent cancel
    When connecting to a hidden wireless service for the first time,
    connmand asks the SSID through the agent api. If the agent returns
    Canceled, return OperationAborted to the caller of Connect() as other
    calls to Connect() behave this way.

--- connman/gsupplicant/supplicant.c
+++ connman/gsupplicant/supplicant.c
@@ -3902,7 +3902,7 @@ int g_supplicant_interface_scan(GSupplicantInterface *interface,
 
 static int parse_supplicant_error(DBusMessageIter *iter)
 {
-	int err = -ECANCELED;
+	int err = -ECONNABORTED;
 	char *key;
 
 	if (!iter)
