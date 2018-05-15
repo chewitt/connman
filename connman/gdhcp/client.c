dc54fd25cf721f35a0f9235ae2f1aec3d23afc4b
Christian Spielberger <christian.spielberger@gmail.com>
2018-05-15 12:35:09 +0200

    dhcp: Use __connman_util_get_random
    In gdhcp replace usage of local functions for random number generation
    by functions in src/util.c. So dhcp_get_random() and
    dhcp_cleanup_random() are removed.
    
    Conflicts:
    	connman/gdhcp/client.c

--- connman/gdhcp/client.c
+++ connman/gdhcp/client.c
@@ -546,7 +546,7 @@ static int send_release(GDHCPClient *dhcp_client,
 	debug(dhcp_client, "sending DHCP release request");
 
 	init_packet(dhcp_client, &packet, DHCPRELEASE);
-	dhcp_get_random(&rand);
+	__connman_util_get_random(&rand);
 	packet.xid = rand;
 	packet.ciaddr = htonl(ciaddr);
 
@@ -1717,7 +1717,7 @@ static gboolean continue_rebound(gpointer user_data)
 	/*recalculate remaining rebind time*/
 	dhcp_client->T2 >>= 1;
 	if (dhcp_client->T2 > 60) {
-		dhcp_get_random(&rand);
+		__connman_util_get_random(&rand);
 		dhcp_client->t2_timeout =
 			connman_wakeup_timer_add_full(G_PRIORITY_HIGH,
 					dhcp_client->T2 * 1000 + (rand % 2000) - 1000,
@@ -1765,7 +1765,7 @@ static gboolean continue_renew (gpointer user_data)
 	dhcp_client->T1 >>= 1;
 
 	if (dhcp_client->T1 > 60) {
-		dhcp_get_random(&rand);
+		__connman_util_get_random(&rand);
 		dhcp_client->t1_timeout =
 			connman_wakeup_timer_add_full(G_PRIORITY_HIGH,
 				dhcp_client->T1 * 1000 + (rand % 2000) - 1000,
@@ -2881,7 +2881,7 @@ int g_dhcp_client_start(GDHCPClient *dhcp_client, const char *last_address)
 		if (re != 0)
 			return re;
 
-		dhcp_get_random(&rand);
+		__connman_util_get_random(&rand);
 		dhcp_client->xid = rand;
 		dhcp_client->start = time(NULL);
 		g_hash_table_remove_all(dhcp_client->secs_bcast_hash);
