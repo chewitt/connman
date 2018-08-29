adc3b6a82a73b398094abf41656370f718526a16
Slava Monich <slava.monich@jolla.com>
2018-08-29 17:43:10 +0300

    service: Initialize order for VPN services
    If the default value of do_split_routing is 0 (false) then
    according to set_split_routing() the default order for VPN
    services should be 10. If the whole structure is zeroed on
    initialization, do_split_routing and order for VPN services
    were left in inconsistent state.
    
    Conflicts:
    	connman/src/service.c

--- connman/src/service.c
+++ connman/src/service.c
@@ -8541,6 +8541,7 @@ __connman_service_create_from_provider(struct connman_provider *provider)
 	if (__connman_service_load_modifiable(service) != 0)
 		g_get_current_time(&service->modified);
 
+	service->order = service->do_split_routing ? 0 : 10;
 	service->favorite = true;
 
 	service->state_ipv4 = service->state_ipv6 = CONNMAN_SERVICE_STATE_IDLE;
