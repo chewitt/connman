53c1dc05f99455321b241b0d160bf9e831a35458
Slava Monich <slava.monich@jolla.com>
2018-09-17 19:48:39 +0300

    service: Expose service identifier to plugins
    Public connman_service_get_identifier() can be called by plugins
    and replaces internal __connman_service_get_ident().

--- connman/src/connman.h
+++ connman/src/connman.h
@@ -721,7 +721,6 @@ struct connman_ipconfig *__connman_service_get_ipconfig(
 				struct connman_service *service, int family);
 bool __connman_service_is_connected_state(struct connman_service *service,
 					enum connman_ipconfig_type type);
-const char *__connman_service_get_ident(struct connman_service *service);
 const char *__connman_service_get_path(struct connman_service *service);
 const char *__connman_service_get_name(struct connman_service *service);
 struct connman_network *__connman_service_get_network(struct connman_service *service);
