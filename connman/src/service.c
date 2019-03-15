6db7b976075c02d36674e09349cfda1010190ca0
Daniel Wagner <wagi@monom.org>
2019-03-15 09:09:47 +0100

    service: Do complete state transition on disconnect with error code.
    Normally, connman_agent_report_error() will report -EINPROGRESS. In this
    case we return early from service_indicate_state() and miss to call
    __connman_notifier_disconnect(). By missing to call
    __connman_notifier_disconnect() we end up in an inconsistent state
    machine.
    
    This was observed when wpa_supplicant reported a disconnect with
    reason code 1 (blocked) and connman_agent_reported_error() return
    -EINPROGRESS.
    
    Reported and tested by Henrik Persson.

--- connman/src/service.c
+++ connman/src/service.c
@@ -7652,15 +7652,14 @@ static int service_indicate_state(struct connman_service *service)
 						service_retry_connect, service);
 		}
 
-		if (service->connect_reason == CONNMAN_SERVICE_CONNECT_REASON_USER &&
+		if (service->connect_reason == CONNMAN_SERVICE_CONNECT_REASON_USER) {
 			connman_agent_report_error(service, service->path,
-					error2string(service->error),
-					report_error_cb,
-					get_dbus_sender(service),
-					NULL) == -EINPROGRESS)
-			return 0;
+						error2string(service->error),
+						report_error_cb,
+						get_dbus_sender(service),
+						NULL);
+		}
 		service_complete(service);
-
 		break;
 	}
 
