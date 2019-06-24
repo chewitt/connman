b4c0bea460c94c4ebf799255d9f1e08103927db9
Jussi Laakkonen <jussi.laakkonen@jolla.com>
2019-06-24 17:37:53 +0300

    vpn: Handle OperationCanceled D-Bus message reply
    OperationCanceled (ECANCELED) is returned by connman-vpnd if the VPN
    agent used to input credentials for a VPN is canceled. This needs to be
    handled in plugins/vpn.c:connect_reply() and with this error
    autoconnect should be disabled for the VPN service.

--- plugins/vpn.c
+++ plugins/vpn.c
@@ -477,6 +477,9 @@ static int errorstr2val(const char *error) {
 	if (g_strcmp0(error, CONNMAN_ERROR_INTERFACE ".AlreadyConnected") == 0)
 		return -EISCONN;
 
+	if (g_strcmp0(error, CONNMAN_ERROR_INTERFACE ".OperationCanceled") == 0)
+		return -ECANCELED;
+
 	return -ECONNREFUSED;
 }
 
@@ -499,7 +502,16 @@ static void connect_reply(DBusPendingCall *call, void *user_data)
 	if (dbus_set_error_from_message(&error, reply)) {
 		int err = errorstr2val(error.name);
 
-		if (err != -EINPROGRESS) {
+		/*
+		 * ECANCELED means that user has canceled authentication
+		 * dialog. That's not really an error, it's part of a normal
+		 * workflow. We also take it as a request to turn autoconnect
+		 * off, in case if it was on.
+		 */
+		if (err == -ECANCELED) {
+			DBG("%s connect canceled", data->path);
+			connman_provider_set_autoconnect(data->provider, false);
+		} else if (err != -EINPROGRESS) {
 			connman_error("Connect reply: %s (%s)", error.message,
 								error.name);
 			DBG("data %p cb_data %p", data, cb_data);
