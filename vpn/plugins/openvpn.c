2b5f5024728c2911dfd8cda71fece603cab81d05
Jussi Laakkonen <jussi.laakkonen@jolla.com>
2019-11-21 14:45:16 +0200

    openvpn: Use correct error value in VPN agent credential reply
    Use the error value set earlier instead of always reporting back EACCES
    to vpn-provider.c:connect_cb().

--- vpn/plugins/openvpn.c
+++ vpn/plugins/openvpn.c
@@ -721,7 +721,7 @@ static void request_input_credentials_reply(DBusMessage *reply,
 	return;
 
 err:
-	ov_connect_done(data, EACCES);
+	ov_connect_done(data, err);
 }
 
 static int request_credentials_input(struct ov_private_data *data)
