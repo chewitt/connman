3a705927f5158b999e6da5f96f9a12e89b52df89
Jussi Laakkonen <jussi.laakkonen@jolla.com>
2019-12-09 15:15:55 +0200

    Add OMP copyright.

--- connman/vpn/vpn-provider.c
+++ connman/vpn/vpn-provider.c
@@ -4,6 +4,7 @@
  *
  *  Copyright (C) 2012-2013  Intel Corporation. All rights reserved.
  *  Copyright (C) 2019  Jolla Ltd.
+ *  Copyright (C) 2019  Open Mobile Platform LLC.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
