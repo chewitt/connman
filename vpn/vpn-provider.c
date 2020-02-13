chewitt/connman

commit 07d35c17e3077ce7133c4ef7c0bc30c2944502ff

vpn-provider: Split nameserver string also by comma
Instead only splitting the nameserver string by space, split also by
comma.

vpn/vpn-provider.c
@@ -2852,7 +2852,7 @@ int vpn_provider_set_nameservers(struct vpn_provider *provider,
 	if (!nameservers)
 		return 0;
 
-	provider->nameservers = g_strsplit(nameservers, " ", 0);
+	provider->nameservers = g_strsplit(nameservers, ", ", 0);
 
 	return 0;
 }

commit 2eebc058c941795593e9afb49249ef4aae32405d

vpn: Split nameservers strings correctly
g_strsplit_set() splits the string into a number of tokens not
containing any of the characters in delimiter.

Fixes: 07d35c17e307 ("vpn-provider: Split nameserver string also by comma")

vpn/vpn-provider.c
@@ -2852,7 +2852,7 @@ int vpn_provider_set_nameservers(struct vpn_provider *provider,
 	if (!nameservers)
 		return 0;
 
-	provider->nameservers = g_strsplit(nameservers, ", ", 0);
+	provider->nameservers = g_strsplit_set(nameservers, ", ", 0);
 
 	return 0;
 }