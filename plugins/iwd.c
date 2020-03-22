chewitt/connman

commit 6b391cfd294d528ed681c3b51c1f3f968784890b

iwd: Synchronize station scanning property with scan state
Release the scan_pending reply after scanning has stopped.

By synchronizing the scanning property of a iwd station with connman
device, it can call the __connman_technology_scan_stopped function so
that the scan_pending is removed from the list.

plugins/iwd.c
@@ -1020,6 +1020,7 @@ static void station_property_change(GDBusProxy *proxy, const char *name,
 		DBusMessageIter *iter, void *user_data)
 {
 	struct iwd_station *iwds;
+	struct iwd_device *iwdd;
 	const char *path;
 
 	path = g_dbus_proxy_get_path(proxy);
@@ -1055,6 +1056,11 @@ static void station_property_change(GDBusProxy *proxy, const char *name,
 
 		if (!iwds->scanning)
 			update_signal_strength(iwds);
+
+		iwdd = g_hash_table_lookup(devices, path);
+		if (iwdd)
+			connman_device_set_scanning(iwdd->device,
+				CONNMAN_SERVICE_TYPE_WIFI, iwds->scanning);
 
 		DBG("%s scanning %d", path, iwds->scanning);
 	}

commit f1444a26bff1b4e3e980007f7fd54845d63825c1

iwd: Mark network avaible after scaning
connman_device_set_scanning() marks all networks as unavailable during
the scan period. The iwd plugin needs mark the network available
again.

Fixes: 6b391cfd294d ("iwd: Synchronize station scanning property with scan state")

plugins/iwd.c
@@ -972,12 +972,15 @@ static void _update_signal_strength(const char *path, int16_t signal_strength)
 
 	connman_network_set_strength(iwdn->network,
 					calculate_strength(signal_strength));
+	connman_network_set_available(iwdn->network, true);
 	connman_network_update(iwdn->network);
 }
 
 static void ordered_networks_cb(DBusMessage *message, void *user_data)
 {
 	DBusMessageIter array, entry;
+	struct iwd_device *iwdd;
+	char *path = user_data;
 
 	DBG("");
 
@@ -1005,6 +1008,11 @@ static void ordered_networks_cb(DBusMessage *message, void *user_data)
 
 		dbus_message_iter_next(&entry);
 	}
+
+	iwdd = g_hash_table_lookup(devices, path);
+	if (iwdd)
+		connman_device_set_scanning(iwdd->device,
+				CONNMAN_SERVICE_TYPE_WIFI, false);
 }
 
 static void update_signal_strength(struct iwd_station *iwds)
@@ -1012,7 +1020,7 @@ static void update_signal_strength(struct iwd_station *iwds)
 	if (!g_dbus_proxy_method_call(iwds->proxy,
 					"GetOrderedNetworks",
 					NULL, ordered_networks_cb,
-					NULL, NULL))
+					g_strdup(iwds->path), g_free))
 		DBG("GetOrderedNetworks() failed");
 }
 
@@ -1054,13 +1062,15 @@ static void station_property_change(GDBusProxy *proxy, const char *name,
 		dbus_message_iter_get_basic(iter, &scanning);
 		iwds->scanning = scanning;
 
-		if (!iwds->scanning)
+		if (iwds->scanning) {
+			iwdd = g_hash_table_lookup(devices, path);
+			if (iwdd)
+				connman_device_set_scanning(iwdd->device,
+					CONNMAN_SERVICE_TYPE_WIFI, true);
+		} else {
 			update_signal_strength(iwds);
+		}
 
-		iwdd = g_hash_table_lookup(devices, path);
-		if (iwdd)
-			connman_device_set_scanning(iwdd->device,
-				CONNMAN_SERVICE_TYPE_WIFI, iwds->scanning);
 
 		DBG("%s scanning %d", path, iwds->scanning);
 	}