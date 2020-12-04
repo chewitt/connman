4e64c0f1702cac1c7dab95ca7a27feb5bccf4ab5
Pieter Cardoen <P.Cardoen@TELEVIC.com>
2020-12-04 13:18:41 +0100

    rtnl: Mark dsa interfaces as ethernet type
    Marvell on-board switch prefixes the interface names with 'dsa'. Mark
    these interfaces as ethernet type.

--- connman/src/rtnl.c
+++ connman/src/rtnl.c
@@ -198,6 +198,9 @@ static void read_uevent(struct interface_data *interface)
 		} else if (strcmp(devtype + 8, "bond") == 0) {
 			interface->service_type = CONNMAN_SERVICE_TYPE_ETHERNET;
 			interface->device_type = CONNMAN_DEVICE_TYPE_ETHERNET;
+		} else if (strcmp(devtype + 8, "dsa") == 0) {
+			interface->service_type = CONNMAN_SERVICE_TYPE_ETHERNET;
+			interface->device_type = CONNMAN_DEVICE_TYPE_ETHERNET;
 		} else {
 			connman_warn("%s DEVTYPE=%s not supported, ignoring",
 					name, devtype);
