024309a9e04a81c2b70b1a2c0efb7185542af5ac
VAUTRIN Emmanuel (Canal Plus Prestataire) <Emmanuel.VAUTRIN@cpexterne.org>
2021-02-10 08:25:49 +0000

    wifi: Reset disconnecting status of any network
    To avoid staying stuck in "Operation aborted" error state when
    connecting, the disconnecting status shall be reset by each disconnect
    callback, whatever network is involved, even if not previously
    connected.
    
    Fixes: dd86f09107e8 ("wifi: Always disconnect connection completely")

--- plugins/wifi.c
+++ plugins/wifi.c
@@ -2266,6 +2266,8 @@ static void disconnect_callback(int result, GSupplicantInterface *interface,
 	if (g_slist_find(wifi->networks, network))
 		connman_network_set_connected(network, false);
 
+	wifi->disconnecting = false;
+
 	if (network != wifi->network) {
 		if (network == wifi->pending_network)
 			wifi->pending_network = NULL;
@@ -2275,7 +2277,6 @@ static void disconnect_callback(int result, GSupplicantInterface *interface,
 
 	wifi->network = NULL;
 
-	wifi->disconnecting = false;
 	wifi->connected = false;
 
 	if (wifi->pending_network) {
