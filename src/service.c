chewitt/connman

commit e89d4abfaf362d4176f7581314b8ede22f4e78bd

service: Clear up connect logic when called from D-Bus
When connected via D-Bus, iterate only over the beginning of the list
where the services are either connected or connecting. If the interface
is not available, disconnect the other service(s) which use the
interface. Once all service(s) have been disconnected, bail out if any
of them was asynchronous and reported -EINPROGRESS. Do not force the
service to idle as it will not be possible to clean up once the
anychronous disconnect completes.

src/service.c
@@ -3953,8 +3953,8 @@ static DBusMessage *connect_service(DBusConnection *conn,
 					DBusMessage *msg, void *user_data)
 {
 	struct connman_service *service = user_data;
+	int err = 0;
 	GList *list;
-	int err;
 
 	DBG("service %p", service);
 
@@ -3969,15 +3969,19 @@ static DBusMessage *connect_service(DBusConnection *conn,
 		 * interfaces for a given technology type (like having
 		 * more than one wifi card).
 		 */
-		if (service->type == temp->type &&
-				is_connecting(temp) &&
-				!is_interface_available(service, temp)) {
-
-			__connman_service_disconnect(temp);
-			set_idle(temp);
+		if (!is_connecting(temp) && !is_connected(temp))
 			break;
+
+		if (service->type != temp->type)
+			continue;
+
+		if(!is_interface_available(service, temp)) {
+			if (__connman_service_disconnect(temp) == -EINPROGRESS)
+				err = -EINPROGRESS;
 		}
 	}
+	if (err == -EINPROGRESS)
+		return __connman_error_in_progress(msg);
 
 	service->ignore = false;
 

commit 10dfc8786a2d33ad8ad31b3b01d8aade5b84dff5

service: Fix disconnection search before connecting
At the beginning of a connection, the search of potential disconnection
shall not stop at the first idle service.

Fixes: e89d4abfaf36 ("service: Clear up connect logic when called from D-Bus")

src/service.c
@@ -4538,7 +4538,7 @@ static DBusMessage *connect_service(DBusConnection *conn,
 		struct connman_service *temp = list->data;
 
 		if (!is_connecting(temp->state) && !is_connected(temp->state))
-			break;
+			continue;
 
 		if (service == temp)
 			continue;