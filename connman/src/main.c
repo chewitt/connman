1d37025e736168a3ace4a4b7daf2a54a3257c3a8
Jussi Laakkonen <jussi.laakkonen@jolla.com>
2021-09-16 15:29:55 +0300

    [connman] Revert "Added support for systemd notify". JB#55244
    This reverts commit 75d55a231c077f651a2c22db9dfa5e1dad99980f.
    
    We do not use the notify type anymore. This is unnecessary change
    causing only conflict with upstream.

--- connman/src/main.c
+++ connman/src/main.c
@@ -42,9 +42,6 @@
 #include <gweb/gweb.h>
 #include <gweb/gresolv.h>
 #include <gdhcp/gdhcp.h>
-#ifdef SYSTEMD
-#include <systemd/sd-daemon.h>
-#endif
 
 #include "connman.h"
 #include "iptables_ext.h"
@@ -661,9 +658,6 @@ static gchar *option_wifi = NULL;
 static gboolean option_detach = TRUE;
 static gboolean option_dnsproxy = TRUE;
 static gboolean option_backtrace = TRUE;
-#ifdef SYSTEMD
-static gboolean option_systemd = FALSE;
-#endif
 static gboolean option_version = FALSE;
 
 static bool parse_debug(const char *key, const char *value,
@@ -731,10 +725,6 @@ static GOptionEntry options[] = {
 	{ "nobacktrace", 0, G_OPTION_FLAG_REVERSE,
 				G_OPTION_ARG_NONE, &option_backtrace,
 				"Don't print out backtrace information" },
-#ifdef SYSTEMD
-	{ "systemd", 0, 0, G_OPTION_ARG_NONE, &option_systemd,
-				"Notify systemd when started"},
-#endif
 	{ "version", 'v', 0, G_OPTION_ARG_NONE, &option_version,
 				"Show version information and exit" },
 	{ NULL },
@@ -1002,14 +992,6 @@ int main(int argc, char *argv[])
 	g_free(option_plugin);
 	g_free(option_nodevice);
 	g_free(option_noplugin);
-
-#ifdef SYSTEMD
-	/* Tell systemd that we have started up */
-	if( option_systemd ) {
-		DBG("Notifying systemd.");
-		sd_notify(0, "READY=1");
-	}
-#endif
 
 	g_main_loop_run(main_loop);
 
