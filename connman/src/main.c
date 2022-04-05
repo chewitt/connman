72ce61f93614b3fe8ee76facffd520bf905bc771
Jussi Laakkonen <jussi.laakkonen@jolla.com>
2022-04-05 13:09:00 +0300

    main: Add path to localtime to config options.
    [main] Add path to localtime to config options. JB#53924
    
    The path to localtime is kept as default /etc/localtime but in some
    cases external daemon manages the localtime by changing its own symlink
    to which /etc/localtime points to. Thus, ConnMan would not see any
    change in the localtime changes.

--- connman/src/main.c
+++ connman/src/main.c
@@ -52,6 +52,7 @@
 #define DEFAULT_STORAGE_DIR_PERMISSIONS (0700)
 #define DEFAULT_STORAGE_FILE_PERMISSIONS (0600)
 #define DEFAULT_UMASK (0077)
+#define DEFAULT_LOCALTIME "/etc/localtime"
 
 #define MAINFILE "main.conf"
 #define CONFIGMAINFILE CONFIGDIR "/" MAINFILE
@@ -97,6 +98,7 @@ static struct {
 	char *vendor_class_id;
 	GHashTable *fallback_device_types;
 	bool enable_login_manager;
+	char *localtime;
 } connman_settings  = {
 	.bg_scan = true,
 	.pref_timeservers = NULL,
@@ -118,6 +120,7 @@ static struct {
 	.vendor_class_id = NULL,
 	.fallback_device_types = NULL,
 	.enable_login_manager = false,
+	.localtime = NULL,
 };
 
 #define CONF_BG_SCAN                    "BackgroundScanning"
@@ -144,6 +147,7 @@ static struct {
 #define CONF_VENDOR_CLASS_ID            "VendorClassID"
 #define CONF_FALLBACK_DEVICE_TYPES      "FallbackDeviceTypes"
 #define CONF_ENABLE_LOGIN_MANAGER       "EnableLoginManager"
+#define CONF_LOCALTIME                  "Localtime"
 
 static const char *supported_options[] = {
 	CONF_BG_SCAN,
@@ -173,6 +177,7 @@ static const char *supported_options[] = {
 	CONF_VENDOR_CLASS_ID,
 	CONF_FALLBACK_DEVICE_TYPES,
 	CONF_ENABLE_LOGIN_MANAGER,
+	CONF_LOCALTIME,
 	NULL
 };
 
@@ -544,14 +549,23 @@ static void parse_config(GKeyFile *config)
 		connman_settings.fallback_device_types =
 				parse_fallback_device_types(str_list, len);
 
+	g_strfreev(str_list);
+
 	g_clear_error(&error);
 
-	boolean = __connman_config_get_bool(config, "General",
+	boolean = __connman_config_get_bool(config, group,
 				CONF_ENABLE_LOGIN_MANAGER, &error);
 	if (!error)
 		connman_settings.enable_login_manager = boolean;
 
-	g_strfreev(str_list);
+	g_clear_error(&error);
+
+	str = __connman_config_get_string(config, group, CONF_LOCALTIME,
+				&error);
+	if (!error)
+		connman_settings.localtime = str;
+	else
+		g_free(str);
 
 	g_clear_error(&error);
 }
@@ -756,6 +770,10 @@ const char *connman_option_get_string(const char *key)
 		return connman_settings.tethering_subnet_block ?
 			connman_settings.tethering_subnet_block :
 			CONF_TETHERING_SUBNET_BLOCK_DEF;
+
+	if (g_str_equal(key, CONF_LOCALTIME))
+		return connman_settings.localtime ?
+				connman_settings.localtime : DEFAULT_LOCALTIME;
 
 	return NULL;
 }
@@ -1064,6 +1082,7 @@ int main(int argc, char *argv[])
 	g_free(connman_settings.tethering_subnet_block);
 	g_free(connman_settings.storage_root);
 	g_free(connman_settings.fs_identity);
+	g_free(connman_settings.localtime);
 
 	if (connman_settings.fallback_device_types)
 		g_hash_table_unref(connman_settings.fallback_device_types);
