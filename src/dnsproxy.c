82ce08135a14ef4ab5b5b60f1e426a26b9fc72fc
Matthias Gerstner <matthias.gerstner@suse.de>
2022-10-27 12:32:59 +0200

    dnsproxy: add my copyright statement covering the larger refactoring changes

--- src/dnsproxy.c
+++ src/dnsproxy.c
@@ -3,6 +3,7 @@
  *  Connection Manager
  *
  *  Copyright (C) 2007-2014  Intel Corporation. All rights reserved.
+ *  Copyright (C) 2022 Matthias Gerstner of SUSE. All rights reserved.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
