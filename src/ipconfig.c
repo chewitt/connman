74e97eda84c4c4fefbd00dceb767495dd062d290
Daniel Wagner <wagi@monom.org>
2022-12-18 20:28:19 +0100

    ipconfig: Do not filter out 0.0.0.0 gateway routes
    This reverts commit cb05780d86c3 ("ipconfig: Don't add invalid gateway
    routes") as it introduced regressions for IPv4LL setups.
    
    0.0.0.0 is a valid gateway address for IPv4LL setups (see forwarding
    rules in RFC3927).
    
    Reported by Geoffrey Van Landeghem <geoffrey.vl@gmail.com>

--- src/ipconfig.c
+++ src/ipconfig.c
@@ -1172,7 +1172,7 @@ int __connman_ipconfig_gateway_add(struct connman_ipconfig *ipconfig)
 
 	DBG("");
 
-	if (!ipconfig->address || !ipconfig->address->gateway)
+	if (!ipconfig->address)
 		return -EINVAL;
 
 	service = __connman_service_lookup_from_index(ipconfig->index);
