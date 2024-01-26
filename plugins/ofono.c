chewitt/connman

commit 054dacc71c6ae4273cdc6463b41bdf2c58b27b52

ofono: Set "powered" property according to value
The "powered" modem property is set according to new value.
Otherwise, connman forces the modem to always be powered.

plugins/ofono.c
@@ -2315,8 +2315,8 @@ static gboolean modem_changed(DBusConnection *conn, DBusMessage *message,
 
 		DBG("%s Powered %d", modem->path, modem->powered);
 
-		if (!modem->powered)
-			modem_set_powered(modem, TRUE);
+		/* Set the powered according to the value */
+		modem_set_powered(modem, powered);
 	} else if (g_str_equal(key, "Online")) {
 		dbus_bool_t online;
 

commit ee0046efd0f36740d0330ffb0146cf4238bff3d6

ofono: Don't invoke SetProperty unnecessarily
When org.ofono.Modem.Powered property changes, connman invokes the
SetProperty method again with the new value.  This is pointless and
only creates unnecessary DBus traffic.

connmand[30]: plugins/ofono.c:modem_set_powered() /phonesim powered 1
connmand[30]: plugins/ofono.c:set_property() /phonesim path /phonesim org.ofono.Modem.Powered
connmand[30]: plugins/ofono.c:set_property_reply() /phonesim path /phonesim org.ofono.Modem.Powered
connmand[30]: plugins/ofono.c:modem_changed() /phonesim Powered 1
connmand[30]: plugins/ofono.c:modem_set_powered() /phonesim powered 1
connmand[30]: plugins/ofono.c:set_property() /phonesim path /phonesim org.ofono.Modem.Powered
connmand[30]: plugins/ofono.c:set_property_reply() /phonesim path /phonesim org.ofono.Modem.Powered

Fixes: 054dacc71c6a ("ofono: Set "powered" property according to value")

plugins/ofono.c
@@ -1969,12 +1969,11 @@ static gboolean modem_changed(DBusConnection *conn, DBusMessage *message,
 		dbus_bool_t powered;
 
 		dbus_message_iter_get_basic(&value, &powered);
+
 		modem->powered = powered;
+		modem->set_powered = powered;
 
 		DBG("%s Powered %d", modem->path, modem->powered);
-
-		/* Set the powered according to the value */
-		modem_set_powered(modem, powered);
 	} else if (g_str_equal(key, "Online")) {
 		dbus_bool_t online;
 